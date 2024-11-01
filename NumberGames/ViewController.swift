7b468460d9e801a9a11e65f88063d7ca19fac7ae
Michelle379 <michelle379123@gmail.com>
2024-11-02 00:26:01 +0800

    Initial Commit

---
+++ NumberGames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NumberGames
+//
+//  Created by 王宜婕 on 2024/11/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
